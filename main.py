ecf49e066042e624633b3410efe467bdb2449e3e
Sean Lee <syjlee04@gmail.com>
2025-04-29 03:29:57 -0400

    Add main file

---
+++ main.py
@@ -0,0 +1,16 @@
+from .model.open_coder import OpenCoder
+
+
+if __name__ == "__main__":
+    # Initialize model
+    model = ...
+
+    # Pipeline should be the model's prompting function that takes in a prompt (str) and outputs a response (str)
+    pipeline = model.pipeline
+
+    # Initialize OpenCoder framework that wraps around the model's prompting function
+    openCoder = OpenCoder(pipeline)
+
+    # Example of generating a response using OpenCoder
+    query = 'How do I fix an index out of range error in Python?'
+    print(openCoder.generate(query))
